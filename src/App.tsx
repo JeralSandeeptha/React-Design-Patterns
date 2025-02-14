921f0318e839f17ac8a593419e0793cae9ce53fe
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-02-15 04:27:46 +0530

    Changed some main files

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import './App.css'
-import Heading from './components/Heading';
-import Paragraph from './components/Paragraph';
-import SubTitle from './components/SubTitle';
-import WithLargeFont from './utils/WithLargeFont';
 
 function App() {
 
-  const LargeParagraph = WithLargeFont(Paragraph);
-  const LargeHeading = WithLargeFont(Heading);
-  const LargeSubTitle = WithLargeFont(SubTitle);
-
   return (
     <>
-      <Heading />
-      <LargeHeading />
-
-      <br />
-
-      <Paragraph />
-      <LargeParagraph />
-
-      <br />
-
-      <SubTitle />
-      <LargeSubTitle />
+      
     </>
   );
 
